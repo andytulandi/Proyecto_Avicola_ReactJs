d892f45351d247ea71aeb77d4059a95157181cec
andytulandi <andrestulandi@gmail.com>
2026-03-19 14:44:35 -0500

    se hacen cambios en la estructura y estilo del proyecto

--- Avisena_col/src/main.jsx
+++ Avisena_col/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Footer from './layouts/Footer.jsx'
+import Header from './layouts/Header.jsx'
+import DashboardHome from './views/dashboard/DashboardHome.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DashboardHome />
   </StrictMode>,
 )
