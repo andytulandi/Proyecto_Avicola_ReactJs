1db431bbeaaaa6088a0e0df654ac10c7a6a3f459
andytulandi <andrestulandi@gmail.com>
2026-05-13 17:43:24 -0500

    subo cambios hechos en el proyecto

--- Avisena_col/src/main.jsx
+++ Avisena_col/src/main.jsx
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import PerfilyConfiguracionview from './views/profile/PerfilyConfiguracionview'
 import Header from './layouts/Header.jsx'
+import Iniciosesion from './views/users/Iniciosesion.jsx'
+import RegisterUserView from './views/users/RegisterUserView.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PerfilyConfiguracionview/>
+    <Iniciosesion/>
+    <RegisterUserView/>
   </StrictMode>,
 )
